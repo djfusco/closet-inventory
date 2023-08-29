d52f4aeef07f3313ff8e7cd26ce65ead97c06027
Dani Alamo <dca5244@psu.edu>
2023-08-29 07:52:04 -0400

    changes

--- src/closet-inventory.js
+++ src/closet-inventory.js
@@ -1,47 +1,74 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 import "./item-card.js";
+import "./search-widget.js";
 
-class ClosetInventory extends LitElement {
+export class ClosetInventory extends LitElement{
   static get tag(){
-    return 'closet-inventory';
+    return 'item-card';
   }
   static get properties(){
     return{
-      items: {type: Array}
-    }
-  }
-
-  static styles = css`
-  :host{
-    display: block;
-  }
-  .wrapper{
-    border: 2px solid black; 
-    display flex; 
-  }
-  .item{
-    display: inline-flex; 
-  }
-  `;
-
-  constructor() {
-    super();
-    this.items = []; 
-    this.updateIventory();
-  }
-
-
-  render() {
-    return html`
-    <div class="wrapper">
-        ${this.items.map(item => html`
-        <div class="item"> 
-          <item-card header="${item.header}" subheader="${item.subheader}" content="${item.content}" image="${item.image}"></item-card>
-        </div>
-        `)}
-    </div>
-  `
-  }
+        schoolBadges: {type: Array}
+    }   
+ }
+ constructor(){
+  super(); 
+  this.schoolBadges = [];
+  this.getSearchResults().then((results) => {
+      this.schoolBadges = results;
+  });
 }
 
-customElements.define('closet-inventory', ClosetInventory);
+async getSearchResults(value = '') {
+  const address = `/api/Badge?search=${value}`;
+  const results = await fetch(address).then((response) => {
+      if (response.ok) {
+          return response.json()
+      }
+      return [];
+  })
+  .then((data) => {
+      return data;
+  });
+
+  return results;
+}
+
+async _handleSearchEvent(e) {
+  const term = e.detail.value;
+  this.schoolBadges = await this.getSearchResults(term);
+}
+
+static get styles(){
+  return css `
+    :host{s
+      display: block;
+    }
+    .wrapper{
+      border: 2px solid black; 
+      display flex; 
+    }
+    .item{
+      display: inline-flex; 
+    }
+  `;
+}
+
+render(){
+  return html`
+   <div class="searchbox">
+      <search-widget @value-changed="${this._handleSearchEvent}"></search-widget>
+   </div>
+   <div class= "wrapper">
+  ${this.schoolBadges.map(item => html`
+  <div class="item">
+  <my-closet header="${item.header}" subheading="${item.subheading}" content="${item.content}" image="${item.image}"></my-closet>
+  </div>
+  `)}
+  </div>
+  `
+}
+
+
+
+}
---
+++ src/search-widget.js
@@ -0,0 +1,45 @@
+import { LitElement, html, css } from 'lit';
+
+export class SearchWidget extends LitElement {
+    static get properties() {
+      return {
+        value: { type: String },
+      }
+    }
+  
+    static get styles() {
+      return css`
+      host:{
+        display: block;
+      }
+      input{
+        border:none;
+        font-family: 'Montserrat', sans-serif;
+        width: 300px;
+        height: 45px;
+        background-color: #F7FAFC;
+      }
+      `;
+    }
+  
+    constructor() {
+      super();
+      this.value = 'Placeholder';
+    }
+  
+    render() {
+      return html`
+        <input type="text" value="${this.value}" @input="${this._handleInput}"/>
+      `;
+    }
+    _handleInput(e) {
+      this.value = e.target.value;
+      this.dispatchEvent(new CustomEvent('value-changed', {
+        detail: {
+          value: this.value,
+        }
+      }));
+    }
+  }
+  
+  customElements.define('search-widget', SearchWidget);
