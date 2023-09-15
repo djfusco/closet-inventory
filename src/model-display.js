f84b6673bffe39af09a36bf4ccc59ab728f65591
Dani Alamo <dca5244@psu.edu>
2023-09-15 11:48:28 -0400

    Render the 3D model

--- src/model-display.js
+++ src/model-display.js
@@ -1,7 +1,22 @@
 import { LitElement, html, css } from "lit";
-import "https://ajax.googleapis.com/ajax/libs/model-viewer/3.1.1/model-viewer.min.js";
+import "@google/model-viewer/dist/model-viewer.js";
 
 export class ModelDisplay extends LitElement{
+    static properties = {
+        text : {type : String},
+        modelsrc : {type : String},
+        image : {type : String},
+        poster : {type : String},
+    }
+
+    constructor(){
+        super();
+        this.text = "Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum";
+        this.modelsrc = "https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb";
+        this.image = "https://modelviewer.dev/shared-assets/environments/moon_1k.hdr";
+        this.poster = "https://modelviewer.dev/shared-assets/models/NeilArmstrong.webp"; 
+    }
+    
     static styles = css`
     main{
         flex-grow : 1; 
@@ -9,7 +24,7 @@ export class ModelDisplay extends LitElement{
     `;
     render(){
         return html`
-        <model-viewer alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum" src="shared-assets/models/NeilArmstrong.glb" ar environment-image="shared-assets/environments/moon_1k.hdr" poster="shared-assets/models/NeilArmstrong.webp" shadow-intensity="1" camera-controls touch-action="pan-y"></model-viewer>
+        <model-viewer alt= ${this.text} src= ${this.modelsrc} ar="ar" environment-image= ${this.image} poster= ${this.poster} shadow-intensity="1" camera-controls="camera-controls" touch-action="pan-y" style="height: 500px;" ar-status="not-presenting" data-hax-layout="true" role="textbox"></model-viewer>
         `
     }
 }
